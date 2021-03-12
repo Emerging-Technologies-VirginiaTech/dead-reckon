97ce0742d71b5e54cc0391c8abea6e9b028f37df
DonatasTamosauskas <donatas.tamosauskas@gmail.com>
2021-03-12 10:47:57 -0500

    Implement block 4

--- mockup/ackermann.py
+++ mockup/ackermann.py
@@ -1,3 +1,12 @@
 #Ackermann calculation mockup in python
 
 
+def block4(r, ts, om_fl, om_fr, om_rl, om_rr):
+    """Compute wheel rotations"""
+    s_fl = r * ts * om_fl
+    s_fr = r * ts * om_fr
+    s_rl = r * ts * om_rl
+    s_rr = r * ts * om_rr
+
+    return s_fl, s_fr, s_rl, s_rr
+
